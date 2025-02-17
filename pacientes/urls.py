410820f3892a2f927561051772660236a935a426
joaovnpt <joaovitorguiotti8@gmail.com>
2025-02-17 15:25:23 -0300

    Iniciar template pacientes

--- pacientes/urls.py
+++ pacientes/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('teste/', 'views/pacientes', name='pacientes')
+    path('teste/', views.pacientes, name='pacientes')
 ]
--- pacientes/views.py
+++ pacientes/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def pacientes(request):
-    return HttpResponse()
+    return render(request, 'pacientes.html')
