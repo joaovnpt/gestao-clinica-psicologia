7ced9608341bc61317d356dcb28ade9b6b3bb07b
joaovnpt <joaovitorguiotti8@gmail.com>
2025-02-20 11:31:55 -0300

    Feat: Capture data from template to views, add empty fields verification and save to db

--- pacientes/views.py
+++ pacientes/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
 from .models import Pacientes
+from django.contrib import messages
+from django.contrib.messages import constants
 
 def pacientes(request):
     queixas = Pacientes.queixa_choices
     if request.method == 'GET':
         return render(request, 'pacientes.html', {'queixas': queixas})
     elif request.method == 'POST':
-        print(request.POST)
-        return HttpResponse('aa')
+        nome = request.POST.get('nome')
+        email = request.POST.get('email')
+        telefone = request.POST.get('telefone')
+        queixa = request.POST.get('queixa')
+        foto = request.FILES.get('foto')
+
+        if len(nome.strip()) == 0 or not foto or len(email.strip()) == 0 or len(telefone.strip()) == 0:
+            messages.add_message(request, constants.ERROR, 'Preencha todos os campos corretamente.')
+            return redirect('pacientes')
+
+    
+    pacientes = Pacientes(
+        nome=nome,
+        email=email,
+        telefone=telefone,
+        queixa=queixa,
+        foto=foto
+    )
+    pacientes.save()
+
+    messages.add_message(request, constants.SUCCESS, 'Cadastro realizado com sucesso')
+    return redirect('pacientes')
